4a7bcd654ac86e166ad0bcf88586ccb1c1d8dffe
maxmax <maxbakaiev@gmail.com>
2024-01-19 03:15:25 +0200

    add bcrypt

---
+++ nest-app/src/users/dto/register-user.dto.ts
@@ -0,0 +1,6 @@
+export class RegisterUserDto {
+  readonly id: number;
+  readonly username: string;
+  readonly email: string;
+  readonly role: string;
+}
--- nest-app/src/users/users.service.ts
+++ nest-app/src/users/users.service.ts
@@ -3,6 +3,7 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { Repository, DeepPartial } from 'typeorm';
 import { User } from './user.entity';
 import { JwtService } from '@nestjs/jwt';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class UsersService {
@@ -21,13 +22,24 @@ export class UsersService {
       throw new NotFoundException('Username or email is already taken');
     }
 
-    const user = this.usersRepository.create(userDto as DeepPartial<User>);
+    const saltRounds = 10;
+    const hashedPassword = await bcrypt.hash(userDto.password, saltRounds);
+
+    const user = new User();
+    user.username = userDto.username;
+    user.email = userDto.email;
+    user.password = hashedPassword;
+    if (userDto.role) {
+      user.role = userDto.role;
+    }
+
     const savedUser = await this.usersRepository.save(user);
+
     return savedUser;
   }
 
   private async comparePasswords(enteredPassword: string, storedPassword: string): Promise<boolean> {
-    return enteredPassword === storedPassword;
+    return bcrypt.compare(enteredPassword, storedPassword);
   }
 
   async login(credentials: any): Promise<{ token: string }> {
