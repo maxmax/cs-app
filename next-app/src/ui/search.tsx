374a863ee39d6721032b35282025d64e87dc4065
maxmax <maxbakaiev@gmail.com>
2024-01-29 00:11:07 +0200

    add debouncing

--- next-app/src/ui/search.tsx
+++ next-app/src/ui/search.tsx
@@ -1,6 +1,7 @@
 'use client';
 import { MagnifyingGlassIcon } from '@heroicons/react/24/outline';
 import { useSearchParams, usePathname, useRouter } from 'next/navigation';
+import { useDebouncedCallback } from 'use-debounce';
 
 export default function Search({ placeholder }: { placeholder: string }) {
 
@@ -8,9 +9,9 @@ export default function Search({ placeholder }: { placeholder: string }) {
   const pathname = usePathname();
   const { replace } = useRouter();
 
-  function handleSearch(term: string) {
+  const handleSearch = useDebouncedCallback((term) => {
     console.log(`Searching... ${term}`);
-    
+
     const params = new URLSearchParams(searchParams);
     if (term) {
       params.set('query', term);
@@ -18,7 +19,7 @@ export default function Search({ placeholder }: { placeholder: string }) {
       params.delete('query');
     }
     replace(`${pathname}?${params.toString()}`);
-  }
+  }, 300);
 
   return (
     <div className="py-8 sm:py-5 lg:max-w-none lg:py-5">
